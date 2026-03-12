43944eb5ace2a2819a53ed8c65dc55b55ee5b857
abhijith-a11y <abhijith@lement8.ae>
2026-03-12 11:09:57 +0530

    first commit

--- wp-content/themes/offload/front-page.php
+++ wp-content/themes/offload/front-page.php
@@ -10,7 +10,55 @@
 get_header();
 ?>
 
-<?php get_template_part('template-parts/Banner'); ?>
+<?php
+$banner_assets = get_template_directory_uri() . '/assets/images';
+$banner_data = array(
+    'title' => 'OFFLOAD MEDIA',
+    'images' => array(
+        array(
+            'src' => $banner_assets . '/img_01.png',
+            'alt' => 'Sports collage image 1',
+        ),
+        array(
+            'src' => $banner_assets . '/img_02.png',
+            'alt' => 'Sports collage image 2',
+        ),
+        array(
+            'src' => $banner_assets . '/img_03.png',
+            'alt' => 'Sports collage image 3',
+        ),
+        array(
+            'src' => $banner_assets . '/img_04.png',
+            'alt' => 'Sports collage image 4',
+        ),
+        array(
+            'src' => $banner_assets . '/img_05.png',
+            'alt' => 'Sports collage image 5',
+        ),
+        array(
+            'src' => $banner_assets . '/img_06.png',
+            'alt' => 'Sports collage image 6',
+        ),
+        array(
+            'src' => $banner_assets . '/img_07.png',
+            'alt' => 'Sports collage image 7',
+        ),
+        array(
+            'src' => $banner_assets . '/img_08.png',
+            'alt' => 'Sports collage image 8',
+        ),
+        array(
+            'src' => $banner_assets . '/img_09.png',
+            'alt' => 'Sports collage image 9',
+        ),
+    ),
+    'video' => array(
+        'src' => $banner_assets . '/home_video.mp4',
+        'poster' => $banner_assets . '/video_thumb.png',
+    ),
+);
+get_template_part('template-parts/Banner', null, $banner_data);
+?>
 
 <?php
 get_footer();
